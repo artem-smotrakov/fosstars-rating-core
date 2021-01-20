ff42338e1b905433094c0f8511d9bd902264973e
Artem Smotrakov <artem.smotrakov@sap.com>
2021-01-20 11:07:41 +0100

    Test committing a test report

--- .github/actions/fosstars-create-single-report/entrypoint.sh
+++ .github/actions/fosstars-create-single-report/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
 REPORT_BRANCH=$1
-PROJECT_SCM_URL=$2
+
+PROJECT_SCM_URL=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY
 
 # for testing
 echo "REPORT_BRANCH = $REPORT_BRANCH"
@@ -11,15 +12,20 @@ echo "GITHUB_WORKSPACE = $GITHUB_WORKSPACE"
 ls $GITHUB_WORKSPACE
 
 # Switch to the Fosstars branch
-#git fetch origin $REPORT_BRANCH || git branch $REPORT_BRANCH
-#git chechout $REPORT_BRANCH
+git fetch origin $REPORT_BRANCH || git branch $REPORT_BRANCH
+git checkout $REPORT_BRANCH
+if [ $? -ne 0 ]; then
+    echo "Could not switch to branch '$REPORT_BRANCH'"
+    echo "Did you fortet to run 'actions/checkout' step in your workflow?"
+    exit 1
+fi
 
 # Generate a report
-#echo "Here will be a report" > README.md
+echo "Here will be a report" > README.md
 
 # Commit the report
-#git config --global user.name "Fosstars"
-#git config --global user.email "fosstars@users.noreply.github.com"
-#git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/${{ github.repository }}
-#git commit -am "Update Fosstars report"
-#git push origin $REPORT_BRANCH
+git config --global user.name "Fosstars"
+git config --global user.email "fosstars@users.noreply.github.com"
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+git commit -am "Update Fosstars report"
+git push origin $REPORT_BRANCH
